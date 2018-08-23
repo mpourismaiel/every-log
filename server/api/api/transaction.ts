d0d1430657c5e855647c81eb6fd282cc0a3a7ed4
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2018-08-24 00:25:51 +0430

    Complete the transaction api router

--- server/api/api/transaction.ts
+++ server/api/api/transaction.ts
@@ -1,35 +1,73 @@
 import models from '../../models';
-import { error } from '../../utils/messages';
+import { error, success } from '../../utils/messages';
 import { isLoggedIn } from '../../utils/policies';
 
-export default apiRouter => {
-  const transactions = apiRouter.route('/transactions');
-  transactions
-    .get(isLoggedIn(), (req, res) => {
-      const { _id } = req.user;
-      models.Transaction.find({ user: _id })
-        .exec()
-        .then(results => {
-          res.json({ transactions: results });
-        })
-        .catch(err => res.status(500).json(error(err)));
+const get = (req, res) => {
+  const { _id } = req.user;
+  models.Transaction.find({ user: _id })
+    .exec()
+    .then(results => {
+      res.json({ transactions: results });
     })
-    .post(isLoggedIn(), (req, res) => {
-      const { price, description, type, category, date } = req.body;
-      const { _id } = req.user;
-      if (!price) {
-        return res.status(403).json(error('Price cannot be empty'));
-      }
+    .catch(err => res.status(500).json(error(err)));
+};
 
-      const data = {
-        price,
-        description,
-        type,
-        category: category || 'No Category',
-        date: date || Date.now(),
-        user: _id,
-      };
-      new models.Transaction(data).save();
-      res.status(200).json(data);
-    });
+const create = (req, res) => {
+  const { price, description, type, category, date } = req.body;
+  const { _id } = req.user;
+  if (!price) {
+    return res.status(403).json(error('Price cannot be empty'));
+  }
+
+  const data = {
+    price,
+    description,
+    type,
+    category: category || 'No Category',
+    date: date || Date.now(),
+    user: _id,
+  };
+  new models.Transaction(data).save();
+  res.status(200).json(data);
+};
+
+const update = (where: 'body' | 'params') => (req, res) => {
+  const { id } = req[where];
+  const { price, description, type, category, date } = req.body;
+  const { _id } = req.user;
+  if (!price) {
+    return res.status(403).json(error('Price cannot be empty'));
+  }
+
+  const data = {
+    price,
+    description,
+    type,
+    category: category || 'No Category',
+    date,
+  };
+  models.Transaction.update({ _id: id, user: _id }, data);
+  res.status(200).json(data);
+};
+
+const deleteOne = (where: 'body' | 'params') => (req, res) => {
+  const { id } = req[where];
+  const { _id } = req.user;
+
+  models.Transaction.deleteOne({ _id: id, user: _id });
+  res.status(200).json(success());
+};
+
+export default apiRouter => {
+  apiRouter
+    .route('/transactions')
+    .get(isLoggedIn(), get)
+    .post(isLoggedIn(), create)
+    .put(isLoggedIn(), update('body'))
+    .delete(isLoggedIn(), deleteOne('body'));
+
+  apiRouter
+    .route('/transactions/:id')
+    .put(isLoggedIn(), update('params'))
+    .delete(isLoggedIn(), deleteOne('params'));
 };
