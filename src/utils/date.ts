6b996ac6ae33413d591c6fb3b081f389431ca332
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2018-09-07 19:17:10 +0430

    Add more options to date function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -18,15 +18,33 @@ export const formatDate = (date: Date, format: string) => {
         ? '0' + date.getDate()
         : date.getDate().toString(),
     )
-    .replace(/%m/g, (date.getMonth() + 1).toString())
     .replace(
       /%mm/g,
       date.getMonth().toString().length === 1
         ? '0' + date.getMonth()
         : date.getMonth().toString(),
     )
+    .replace(/%m/g, (date.getMonth() + 1).toString())
     .replace(/%y/g, date.getFullYear().toString())
+    .replace(
+      /%HH/g,
+      date.getHours().toString().length === 1
+        ? '0' + date.getHours()
+        : date.getHours().toString(),
+    )
     .replace(/%H/g, date.getHours().toString())
+    .replace(
+      /%MM/g,
+      date.getMinutes().toString().length === 1
+        ? '0' + date.getMinutes()
+        : date.getMinutes().toString(),
+    )
     .replace(/%M/g, date.getMinutes().toString())
+    .replace(
+      /%SS/g,
+      date.getSeconds().toString().length === 1
+        ? '0' + date.getSeconds()
+        : date.getSeconds().toString(),
+    )
     .replace(/%S/g, date.getSeconds().toString());
 };
